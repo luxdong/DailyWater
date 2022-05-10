5188103796ef2cf0e56df2bc62e180d0ae6f4b70
lux <lucda@163.com>
2022-05-10 23:54:39 +0800

    [ADD] create new project,springboot 2.6.7

---
+++ src/main/java/com/oslu/dailywater/DailywaterApplication.java
@@ -0,0 +1,13 @@
+package com.oslu.dailywater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailywaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailywaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oslu/dailywater/DailywaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslu.dailywater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailywaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
